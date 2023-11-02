3f8558016620cc88590d7f642378630c6b77c593
이아름 <a0534123@naver.com>
2023-11-02 13:49:37 +0900

    memo

--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -1,6 +1,7 @@
 package com.memo.post.bo;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,14 @@ public class PostBO {
 	// input:userId		output:List<Post>
 	public List<Post> getPostListByUserId(int userId){
 		return postMapper.selectPostListByUserId(userId);
+	}
+	
+	// input: 파라미터들  output:x
+	public void addPost(int userId, String subject, String content){
+		String imagePath = null;
+		
+		// TODO : 이미지가 있으면 업로드 
+		
+		postMapper.insertPost(userId, subject, content, imagePath);
 	}
 }
